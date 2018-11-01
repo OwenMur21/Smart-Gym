296602f29b175deb48e620427ca8d406a743c503
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-11-01 17:37:21 +0300

    chages

--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect , get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from .models import *
+# from .forms import *
+
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def gymprofile(request):
+    current_user = request.user
+    current_manager = request.user.gymmanager
+
+    gym = Gym.objects.get(manager = current_user)
+    return render(request, 'templates/gym/gymprofile.html, {'gym':gym})
+
