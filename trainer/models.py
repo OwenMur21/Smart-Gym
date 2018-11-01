77045ba9434d2a1468f6db0fbae9f26f5fa55621
Owenmur21 <owenmuriithi@gmail.com>
2018-11-01 19:23:22 +0300

    Edits trainer

--- trainer/models.py
+++ trainer/models.py
@@ -1,16 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
-from ..gym.models import Gym
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from gym.models import Gym
 
 
 #Create your models here.
 class Trainer(models.Model):
     name = models.CharField(max_length =30)
     bio = models.CharField(max_length =30)
-    location= models.ForeignKey(location)
+    # location= models.ForeignKey(location)
     gym = models.ForeignKey(Gym)
     specially=models.CharField(max_length =30)
     year_Experience=models.CharField(max_length =30)
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User)
+
+
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Trainer.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.trainer.save()
+
+    post_save.connect(save_user_profile, sender=User)
+
 
    
     def __str__(self):
