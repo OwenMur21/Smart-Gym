ace428e8a58b97b54969e0e052aa14f23ce412fe
Owenmur21 <owenmuriithi@gmail.com>
2018-11-01 15:35:59 +0300

    Creates models

--- client/models.py
+++ client/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Class that contains Profile details
+    """
+    name = models.CharField(max_length = 30)
+    profile_pic = models.ImageField(upload_to = 'images/', blank=True)
+    goals = (
+    ('Losing weight', 'Losing weight'),
+    ('Getting toned', 'Getting toned'),
+    ('Getting bigger', 'Getting bigger'),
+    ('Maintain fitness', 'Maintain fitness'),
+    )
+
+    goal = models.CharField(max_length = 30, choices=goals)
+    contact = models.CharField(max_length = 30, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True,)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    post_save.connect(save_user_profile, sender=User)
+
+
+
+    def __str__(self):
+        return self.name
+
+    def save_trainee(self):
+        self.save()
+
+    def delete_trainee(self):
+        self.delete()
+
+
+
+
+
